2412b460cca8808f08e48eb0157e256e387c1379
homervador <pierre.puss@gmail.com>
2016-12-13 10:58:06 +0100

    ajout des tables Client/Commande/LigneDeCommande

--- exo_produit.sql
+++ exo_produit.sql
@@ -1,11 +1,11 @@
 CREATE TABLE Categorie(
-	id INTEGER NOT NULL PRIMARY KEY,
+	id INT NOT NULL IDENTITY PRIMARY KEY,
 	libelles VARCHAR(50),
 	
 );
 
 CREATE TABLE Produit(
-	id INTEGER NOT NULL PRIMARY KEY,
+	id INT NOT NULL IDENTITY PRIMARY KEY,
 	nom VARCHAR(50),
 	code Char(5) UNIQUE,
 	idCategorie INTEGER,
@@ -13,3 +13,35 @@ CREATE TABLE Produit(
 		FOREIGN KEY (idCategorie)
 		REFERENCES Categorie (id)
 );
+
+CREATE TABLE Client(
+	id INT NOT NULL IDENTITY PRIMARY KEY,
+	nom VARCHAR(50)NOT NULL,
+	prénom VARCHAR(50) NOT NULL, 
+	mail VARCHAR(70) UNIQUE NOT NULL, 
+);
+
+CREATE TABLE Commande(
+	id INT NOT NULL IDENTITY PRIMARY KEY,
+	adresseDeLivraison VARCHAR(100)NOT NULL,
+	idClient INTEGER,
+	CONSTRAINT commande_client_fk
+		FOREIGN KEY (idClient)
+		REFERENCES Client (id),
+);
+
+CREATE TABLE LigneDeCommande(
+	id INT NOT NULL IDENTITY PRIMARY KEY,
+	quantite INT NOT NULL,
+	idProduit INTEGER,
+	idCommande INTEGER,
+	CONSTRAINT ligneDeCommande_produit_fk
+		FOREIGN KEY (idProduit)
+		REFERENCES Produit (id),
+	CONSTRAINT ligneDeCommande_commande_fk
+		FOREIGN KEY (idCommande)
+		REFERENCES Commande (id)
+);
+
+
+
